3d7f50d4fb64d70ce41186888526985f4b9d44d8
Maxime Burlat <maximeburlat@gmail.com>
2018-05-20 18:27:13 -0500

    added basic code for server.js and api-routes.js

---
+++ routes/api-routes.js
@@ -0,0 +1,34 @@
+// *****************************************************************************
+// **** api-routes.js - this file offers a set of routes for displaying and
+// saving data to the db
+// ******************************************************************************
+// *** Dependencies
+
+// Requiring our models
+var db = require("../models");
+
+// Routes =============================================================
+module.exports = function(app) {
+  // GET route for getting all of the boilerplates
+  app.get("/api/boilerplates", function(req, res) {
+    // findAll returns all entries for a table when used with no options
+    db.Boilerplate.findAll({}).then(function(bp) {
+      // We have access to the boilerplates as an argument inside of the callback function
+      res.json(bp);
+    });
+  });
+
+  // POST route for saving a new boilerplate
+  app.post("/api/boilerplates", function(req, res) {
+    // create takes an argument of an object describing the item we want to insert
+    // into our table. In this case we just we pass in an object with a text and
+    // complete property
+    db.Boilerplate.create({
+      title: req.body.title,
+      content: req.body.content
+    }).then(function(bp) {
+      // We have access to the new boilerplate as an argument inside of the callback function
+      res.json(bp);
+    });
+  });
+};
