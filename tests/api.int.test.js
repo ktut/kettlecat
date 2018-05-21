c39145634f02fd4525ce972cfd50733e2776f7c3
Maxime Burlat <maximeburlat@gmail.com>
2018-05-21 18:02:10 -0500

    added some tests titles (possible to do that to precise the specs)

--- tests/api.int.test.js
+++ tests/api.int.test.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const api = require("./../routes/api-routes");
 const db = require("./../models");
 
+// TODO we could set a test database to get precise results, if we control exactly the content of the database
+
 test("the created boilerplate is returned once stored", () => {
   expect.assertions(1);
   return api.createBoilerplate("test title", "test content").then(data => {
@@ -23,3 +25,13 @@ test("an empty search returns every boilerplate", () => {
     });
   });
 });
+
+test("a search with one tag returns all elements having this tag");
+
+test(
+  "a search with several tags returns all elements having all tags (the intersection)"
+);
+
+test(
+  "a search with a searchInput shall return all elements that match the searchInput (title or content)"
+);
