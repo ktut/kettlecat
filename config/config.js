b11991be1b9d415c25376035295e8331b15f1b33
Maxime Burlat <maximeburlat@gmail.com>
2018-05-23 17:12:45 -0500

    change the config to use local db

--- config/config.js
+++ config/config.js
@@ -1,16 +1,16 @@
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DB,
-    host: process.env.DB_HOST,
+    username: process.env.DB_DVPT_USER,
+    password: process.env.DB_DVPT_PASS,
+    database: process.env.DB_DVPT_DB,
+    host: process.env.DB_DVPT_HOST,
     dialect: "mysql"
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DB,
-    host: process.env.DB_HOST,
+    username: process.env.DB_DVPT_USER,
+    password: process.env.DB_DVPT_PASS,
+    database: process.env.DB_DVPT_DB,
+    host: process.env.DB_DVPT_HOST,
     dialect: "mysql"
   },
   production: {
