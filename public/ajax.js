f7943b969e3b3f3dd6b94feb8150ab21dd212255
Robertas Plynius <robertasplynius@yahoo.com>
2018-05-30 21:32:23 -0500

    calls

---
+++ public/ajax.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $.ajax({
+    url: queryURL, ///api/boilerplates?
+    method: "GET"
+  }).then(function(data) {
+    $.post(".card", newcard);
+    console.log("add.html", data);
+  });
+});
+//     $(".tag").append($(p));
+//     $(".title").append($(p));
+//     $(".author").append($(p));
+//     $(".desc").append($(p));
+//     $(".lang").append($(p));
+//     $(".content").append($(p));
+//     $(".js-copy").append((p));
+//   });
+// });
