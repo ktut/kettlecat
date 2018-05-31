f0fbe5f4b3ad0a509163fbe3f0e927931a1e4f23
Madeline Story <madeline1093@gmail.com>
2018-05-30 21:27:03 -0500

    api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,6 +14,16 @@ module.exports = {
     // TODO complete implementation
     return db.Boilerplate.findAll({});
   },
+  
+  findOneBoilerplate: (id) => {
+    return db.Boilerplate.findOne({
+      where: {
+        id: id
+      }
+    }).then(bpOne => {
+      return bpOne
+    }) 
+  },
   // due to its dependence to db connection, this function is tested in api.int.test.js
   createBoilerplate: (title, content, tags) => {
     // FIXME tags management makes the test fail, pb of asynchronism
@@ -31,6 +41,28 @@ module.exports = {
       });
     }
   },
+  // update boilerplate
+  updateBoilerPlate: (id, updatedContent) => {
+    return db.Boilerplate.update(updatedContent,
+      {
+        where:{
+          id: id
+        }
+      }).then(bpUpdate => {
+        return bpUpdate
+      });
+  },
+  //delete Boilerplate
+  deleteBoilerPlate: id => {
+    return db.Boilerplate.destroy({
+      where: {
+        id: id
+      }
+    }).then(boilerplate => {
+      //console.log(boilerplate);
+      return boilerplate;
+    });
+  },
   // due to its dependence to db connection, this function is tested in api.int.test.js
   createTag: (title, color) => {
     return db.Tag.create({
@@ -38,6 +70,30 @@ module.exports = {
       color: color
     });
   },
+
+  findAllTags: (title, color) => {
+    return db.Tag.findAll({});
+  },
+
+  deleteTag: (id) => {
+    return db.Tag.destroy({
+      where: {
+        id: id
+      }
+    }).then(tagDel => {
+      return tagDel
+    });
+  },
+
+  updateTag: (id, tagContent) => {
+    return db.Tag.update(tagContents, 
+    {
+      id: id
+    }).then(tagUpdate => {
+      return tagUpdate
+    });
+  },
+
   // this function will only be tested through UI testing
   routes: function(app) {
     // GET route for getting all of the boilerplates
@@ -50,6 +106,11 @@ module.exports = {
       });
     });
 
+    app.get("/api/boilerplates/:id", function(req, res) {
+      this.findOneBoilerplate(req.params.id).then(function(bpOneGet){
+        res.json(bpOneGet);
+      })
+    })
     // POST route for saving a new boilerplate
     app.post("/api/boilerplates", function(req, res) {
       // create takes an argument of an object describing the item we want to insert
@@ -65,6 +126,19 @@ module.exports = {
       });
     });
 
+    //DELETE route for Boilerplate
+    app.delete("/api/boilerplates/:id", function(req, res) {
+      this.deleteBoilerPlate(req.params.id).then(dbreturn => {
+        res.json(dbreturn);
+      });
+    });
+    //PUT route for updating Boilerplates
+    app.put("/api/boilerplates/:id", function(req, res){
+      this.updateBoilerPlate(req.params.id, req.body).then(bpUpdatePut => {
+        res.json(bpUpdatePut);
+      })
+    });
+    ///////////////////////////tag routes///////////////////////////////////
     //POST route for saving a new Tag
     app.post("/api/tags", function(req, res) {
       // create takes an argument of an object describing the item we want to insert
@@ -75,5 +149,24 @@ module.exports = {
         res.json(tag);
       });
     });
+    //GET route for all of the Tags
+    app.get("/api/tags", function(req, res){
+      this.findAllTags(req.body.title, req.body.color).then(function(allTags) {
+        res.json(allTags);
+      });
+    });
+    //DELETE route for a Tag by ID
+    app.delete("api/tags/:id", function(req, res) {
+      this.deleteTag(req.params.id).then(tagreturn => {
+        res.json(tagreturn);
+      })
+    });
+
+    app.put("api/tags/id:", function(req, res) {
+      this.updateTag(req.params.id, req.body).then(tagUpdatePut => {
+        req.json(tagUpdatePut);
+      })
+    });
+
   }
 };
