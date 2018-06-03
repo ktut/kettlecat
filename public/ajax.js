16fd2df5b2697fcb5d1c2acf35215c419893f9d8
Maxime Burlat <maximeburlat@gmail.com>
2018-06-02 19:40:19 -0500

    added ajax functions for boilerplates

--- public/ajax.js
+++ public/ajax.js
@@ -21,11 +21,6 @@
 //   console.log(data);
 //   )};
 
-var getAllPosts = function() {
-  $.get("./api/boilerplates/", function(data) {
-    console.log(data);
-  });
-};
 // var deletePost = function() {
 // $.destroy("./api/boilerplates/:id", data => {
 //   console.log("it worked");
@@ -57,3 +52,81 @@ var getAllPosts = function() {
 // $.put("/api/tags/id:", data => {
 //   console.log("it worked");
 // });
+
+const getAllBoilerplates = function(cb) {
+  $.get("./api/boilerplates/", function(data) {
+    // returns an array containing all Boilerplates
+    cb(data);
+  });
+};
+
+const postBoilerplate = function(boilerplate, cb) {
+  $.post(
+    "./api/boilerplates",
+    {
+      title: boilerplate.title,
+      description: boilerplate.description,
+      lang: boilerplate.lang,
+      content: boilerplate.content,
+      tags: boilerplates.tags
+    },
+    function(result) {
+      cb(result);
+    }
+  );
+};
+
+const putBoilerplate = function(boilerplate, cb) {
+  const apiUrl = "./api/boilerplates/" + boilerplate.id;
+  $.ajax({
+    url: apiUrl,
+    type: "PUT",
+    data: {
+      title: boilerplate.title,
+      description: boilerplate.description,
+      lang: boilerplate.lang,
+      content: boilerplate.content,
+      tags: boilerplates.tags
+    },
+    success: function(result) {
+      cb(result);
+    }
+  });
+};
+
+const putUpVote = function(boilerplate, cb) {
+  const apiUrl = `/api/boilerplates/${
+    boilerplate.id
+  }/upvotes/${boilerplate.upvotes + 1}`;
+  $.ajax({
+    url: apiUrl,
+    type: "PUT",
+    success: function(result) {
+      cb(result);
+    }
+  });
+};
+
+const putDownVote = function(boilerplate, cb) {
+  const apiUrl = `/api/boilerplates/${
+    boilerplate.id
+  }/downvotes/${boilerplate.downvotes + 1}`;
+  $.ajax({
+    url: apiUrl,
+    type: "PUT",
+    success: function(result) {
+      cb(result);
+    }
+  });
+};
+
+const deleteBoilerplate = function(boilerplate, cb) {
+  const apiUrl = `/api/boilerplates/${boilerplate.id}`;
+  $.ajax({
+    url: apiUrl,
+    type: "DELETE",
+    success: function(result) {
+      cb(result);
+    }
+  });
+};
